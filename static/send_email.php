715947afe0fd38e7fa44945e4cdfafce93892d40
Salim <otmanine.salim@gmail.com>
2023-05-25 21:53:30 +0100

    test php email

--- static/send_email.php
+++ static/send_email.php
@@ -1,47 +1,25 @@
 <?php
-
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\SMTP;
-use PHPMailer\PHPMailer\Exception;
-
-require_once "/vendor/autoload.php";
-
-$mail = new PHPMailer;
-
-try {
-    $mail->isSMTP();
-    $mail->Host = 'mail.2060.io';
-    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
-    $mail->Port = 465;
-
+if (isset($_POST['email'])) {
     $name = htmlspecialchars($_POST['name']);
     $phone = htmlspecialchars($_POST['phone']);
     $email = htmlspecialchars($_POST['email']);
     $message = htmlspecialchars($_POST['message']);
 
-    $mail->From = $email;
-    $mail->FromName = $name;
+    $mailFrom = "From: " . $email . " Name: " . $name;
 
-    $mail->addAddress("otmanine.salim@gmail.com");
+    $mailTo = "otmanine.salim@gmail.com";
 
-    $mail->isHTML(true);
+    $mailSubject = "Website contact form";
+    $mailBody = "
+        Name : " . $name . " \n
+        Phone: " . $phone . " \n
+        Email: " . $email . " \n
+        Message: " . $message;
 
-    $mail->Subject = "Website contact form";
-    $mail->Body = "
-        <p>Name : " . $name . "</p>
-        <p>Phone: " . $phone . "</p>
-        <p>Email: " . $email . "<br/></p>
-        <p>Message: " . $message . "</p>"
-    ;
-    $mail->AltBody = $message;
-
-    $mail->send();
-    echo '<script language="javascript" type="text/javascript">';
-    echo 'window.location.replace("succes");';
-    echo '</script>';
-} catch (Exception $e) {
-    echo '<script language="javascript" type="text/javascript">';
-    echo 'window.location.replace("error");';
-    echo '</script>';
+    if (mail($mailTo, $mailSubject, $mailBody, $mailFrom)) {
+        echo "Message has been sent successfully";
+    } else {
+        echo "Mailer Error";
+    }
 }
 ?>
