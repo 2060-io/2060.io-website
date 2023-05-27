bbc2885e3e0d6b1f97c95f2e67f017fa60f6cb78
Salim <otmanine.salim@gmail.com>
2023-05-27 02:42:00 +0100

    fix newsletter

--- static/newsletter.php
+++ static/newsletter.php
@@ -2,13 +2,12 @@
 if (isset($_POST['email'])) {
     $email = htmlspecialchars($_POST['email']);
 
-    $mailFrom = "From: <" . $email . ">";
+    $mailFrom = "From: " . $email . "<" . $email . ">";
 
     $mailTo = "otmanine.salim@gmail.com";
 
-    $mailSubject = "Newsletter inscription";
-    $mailBody = "
-        Email: " . $email;
+    $mailSubject = "Website contact form";
+    $mailBody = "Email: " . $email;
 
     if (mail($mailTo, $mailSubject, $mailBody, $mailFrom)) {
         echo 'success';
